e613b1db0e8934561dcddfa6a3832caefd676150
sande2jm <sande2jm@gmail.com>
2018-08-22 14:21:50 -0400

    version 0.6

--- Testing/Unit/CreateUserService/testing_temp_delete_me.py
+++
@@ -1,7 +0,0 @@
-from subprocess import call
-
-cmds = ['git submodule foreach git add * ', 
-		'git submodule foreach git commit -m test',
-		'git submodule foreach git push origin master']
-for cmd in cmds:
-	call(cmd.split(' '))
