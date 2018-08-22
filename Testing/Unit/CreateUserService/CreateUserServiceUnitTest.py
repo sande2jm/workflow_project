6c738aeb4d3564e911215efc10f3d622ee872deb
sande2jm <sande2jm@gmail.com>
2018-08-22 14:17:29 -0400

    removed delete me file

--- Testing/Unit/CreateUserService/CreateUserServiceUnitTest.py
+++ Testing/Unit/CreateUserService/CreateUserServiceUnitTest.py
@@ -7,30 +7,35 @@ class CreateUserServiceUnitTest(unittest.TestCase):
     
 	def setUp(self):
 		self.config = CreateUserServiceDockerImageCreator()
-		# self.config.getStateFromRepo()
-		# self.config.run()
-		# self.config.updateVersion()
-
-	# def load_docker_image(self):
-	# 	cmd = 'docker pull '+ self.config.username + '/' + self.config.imageName + ':' + self.config.tag
-	# 	call(cmd.split(' '))
-
-	# def run_docker_image(self):
-	# 	cmd = 'docker run -d -p 4000:80 ' + self.config.imageName + ':' + self.tag
-	# 	call(cmd.split(' '))
-	def test_change_state_and_push(self):
 		self.config.getStateFromRepo()
 		self.config.updateVersion()
-		self.config.cleanupStateRepo()
-		prevTag = float(self.config.tag)
-		self.config.getStateFromRepo()
-		self.assertEqual(self.config.tag, str(prevTag + 0.1))
+		self.config.create_docker_image_and_push()
+		self.load_docker_image()
+
+	def load_docker_image(self):
+		cmd = 'docker pull '+ self.config.username + '/' + self.config.imageName + ':' + self.config.tag
+		call(cmd.split(' '))
+
+	def run_docker_image(self):
+		cmd = 'docker run -d -p 4000:80 ' + self.config.imageName + ':' + self.tag
+		call(cmd.split(' '))
+	# def test_change_state_and_push(self):
+	# 	self.config.getStateFromRepo()
+	# 	self.config.updateVersion()
+	# 	self.config.cleanupStateRepo()
+	# 	prevTag = float(self.config.tag)
+	# 	self.config.getStateFromRepo()
+	# 	self.assertEqual(self.config.tag, str(prevTag + 0.1))
+
+	def test_docker_image_is_created(self):
+		cmd = 'docker images ls ' 
+		print(check_output(cmd.split(' ')))
 
 	def tearDown(self):
 		self.config.cleanupStateRepo()
 
-	# def test_service_is_running(self):
-	# 	self.assertEqual(1,1)
+	def test_service_is_running(self):
+		self.assertEqual(1,1)
 
 	# def test_tag_value(self):
 	# 	self.assertEqual(self.config.tag, '0.1')	
